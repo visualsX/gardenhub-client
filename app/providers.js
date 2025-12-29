0470289324665fe3d9f3aa69fd6fbefe2cce5cad
osamajavaid <iosamajavaid@gmail.com>
2025-12-29 11:42:17 +0500

    feat: loading bar

--- app/providers.js
+++ app/providers.js
@@ -4,12 +4,19 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import NextTopLoader from 'nextjs-toploader';
 
 export default function Providers({ children }) {
     const [queryClient] = useState(() => new QueryClient())
 
     return (
         <AntdRegistry>
+            <NextTopLoader
+                color="var(--primary-green)"
+                initialPosition={0.08}
+                crawlSpeed={200}
+                height={3}
+                showSpinner={false} />
             <QueryClientProvider client={queryClient}>
                 {children}
                 <ReactQueryDevtools initialIsOpen={false} />
