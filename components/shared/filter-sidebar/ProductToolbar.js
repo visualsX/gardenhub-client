cd747653de846f6ebfc9b9f7c1003b42d634eaef
osamajavaid <iosamajavaid@gmail.com>
2026-01-11 19:10:43 +0500

    fix: sort by

--- components/shared/filter-sidebar/ProductToolbar.js
+++ components/shared/filter-sidebar/ProductToolbar.js
@@ -58,10 +58,10 @@ export default function ProductToolbar({
             className="custom-select"
             options={[
               { value: 'newest', label: 'Newest' },
-              { value: 'name-asc', label: 'Name: A to Z' },
-              { value: 'name-desc', label: 'Name: Z to A' },
-              { value: 'price-asc', label: 'Price: Low to High' },
-              { value: 'price-desc', label: 'Price: High to Low' },
+              { value: 'name_asc', label: 'Name: A to Z' },
+              { value: 'name_desc', label: 'Name: Z to A' },
+              { value: 'price_asc', label: 'Price: Low to High' },
+              { value: 'price_desc', label: 'Price: High to Low' },
               { value: 'popular', label: 'Popular' },
             ]}
           />
