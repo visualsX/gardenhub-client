964df65f3e05d55e89be0c0e90bbe1828e2c597b
osamajavaid <iosamajavaid@gmail.com>
2026-01-22 10:48:03 +0500

    coupon validation

--- hooks/useOrder.js
+++ hooks/useOrder.js
@@ -1,6 +1,17 @@
 import { useQuery, useMutation } from '@tanstack/react-query';
 import client from '@/lib/api/client-config/client';
+import graphqlClient from '@/lib/api/client-config/graphql-client';
+import { ORDERS_QUERIES } from '@/lib/api/queries/orders.queries';
 import { API_ENDPOINTS } from '@/lib/const/endpoints';
+
+export const useValidateCoupon = () => {
+  return useMutation({
+    mutationFn: async (variables) => {
+      const data = await graphqlClient.request(ORDERS_QUERIES.VALIDATE_COUPON_CODE, variables);
+      return data.validateCoupon;
+    },
+  });
+};
 
 export const usePaymentMethods = () => {
   return useQuery({
