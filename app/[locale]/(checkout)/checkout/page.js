c3d71e0d0f381b1a210d281f9226c0ab7cffb862
osamajavaid <iosamajavaid@gmail.com>
2026-01-25 19:09:35 +0500

    strip wrapped

--- app/[locale]/(checkout)/checkout/page.js
+++ app/[locale]/(checkout)/checkout/page.js
@@ -1,8 +1,13 @@
 import CheckoutPage from '@/components/pages/checkout/CheckoutPage';
+import CheckoutWrapper from '@/components/pages/checkout/CheckoutWrapper';
 import { getCustomerProfile } from '@/lib/api/ssr-calls/server-checkout-profile';
 
 export default async function Checkout() {
   const customerProfile = await getCustomerProfile();
 
-  return <CheckoutPage customerProfile={customerProfile} />;
+  return (
+    <CheckoutWrapper>
+      <CheckoutPage customerProfile={customerProfile} />
+    </CheckoutWrapper>
+  );
 }
