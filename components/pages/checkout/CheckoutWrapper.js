af01ab5a8bd0e8ab29aa8177e9947bcaf46626be
osamajavaid <iosamajavaid@gmail.com>
2026-01-25 19:09:07 +0500

    stripe wrapper added

---
+++ components/pages/checkout/CheckoutWrapper.js
@@ -0,0 +1,12 @@
+'use client';
+
+import { Elements } from '@stripe/react-stripe-js';
+import stripePromise from '@/lib/stripe';
+
+export default function CheckoutWrapper({ children }) {
+    return (
+        <Elements stripe={stripePromise}>
+            {children}
+        </Elements>
+    );
+}
